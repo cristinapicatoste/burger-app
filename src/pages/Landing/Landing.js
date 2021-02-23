1c6197043301aa6a2e126500150bdb9090aaf9f7
Cris <cristina.picatoste@gmail.com>
2021-02-23 21:02:41 +0100

    Actualización nombre aplicación y estilos

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -5,7 +5,8 @@ export const Landing = () => {
     return (
         <div className={styles["Landing-wrap"]}>
             <div className={styles["Landing-content"]} >
-                <h1>Top World Burgers</h1>
+                <h1>Bourmet</h1>
+                <h4>The Best Gourmet Burgers</h4>
             </div>
         </div>
     )
